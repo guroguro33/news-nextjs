2ccfd335aa78e7d92049998ed6cdc526e5e25755
Toru <t02f447f@gmail.com>
2022-01-31 23:18:56 +0900

    create header and layout components

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.scss"
+import Image from "next/image"
+import Link from "next/link"
+
+function Header():JSX.Element {
+    return (
+        <section className="{style.container}"></section>
+    );
+}
+
+export default Header
