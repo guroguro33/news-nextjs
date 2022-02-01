8e33153f2f0af0d665fbcccf569d552f3d607fcf
Toru <t02f447f@gmail.com>
2022-02-02 06:08:46 +0900

    implement top page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,28 @@ import Link from "next/link"
 
 function Header():JSX.Element {
     return (
-        <section className="{style.container}"></section>
+        <section className={styles.container}>
+            <header className={styles.header}>
+                <div className={styles.header__icon}>
+                    <Image 
+                        src="/img/headerIcon/menu.png"
+                        alt="menu icon"
+                        loading="eager"
+                        width={35}
+                        height={35}
+                        priority
+                    />
+                </div>
+                <h1 style={{ letterSpacing: "1px", textAlign: "left" }}>
+                    <Link href="/">
+                        <a href="">
+                            <span style={{ fontWeight: 250 }}>Simple</span>
+                            <span style={{ fontWeight: 100 }}>News</span>
+                        </a>
+                    </Link>
+                </h1>
+            </header>
+        </section>
     );
 }
 
